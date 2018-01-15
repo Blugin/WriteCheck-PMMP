f2e8358b89ae7caf17abd107a162dded0bf6b59f
PresentKim <qjrmckd@gmail.com>
2018-01-15 22:24:33 +0900

    Send help message at left click

--- src/presentkim/writecheck/listener/PlayerEventListener.php
+++ src/presentkim/writecheck/listener/PlayerEventListener.php
@@ -23,19 +23,23 @@ class PlayerEventListener implements Listener{
 
     /** @param PlayerInteractEvent $event */
     public function onPlayerInteractEvent(PlayerInteractEvent $event){
-        if ($event->getAction() === PlayerInteractEvent::RIGHT_CLICK_AIR) {
+        if ($event->getAction() === PlayerInteractEvent::LEFT_CLICK_BLOCK || $event->getAction() === PlayerInteractEvent::RIGHT_CLICK_AIR) {
             $player = $event->getPlayer();
             $inventory = $player->getInventory();
             $item = $inventory->getItemInHand();
             if ($item->getId() == Item::PAPER && $item->getDamage() === 0xff) {
                 $amount = $item->getNamedTag()->getTagValue('whitecheck-amount', IntTag::class, -1);
                 if ($amount !== -1) {
-                    $item->count = 1;
-                    $inventory->removeItem($item);
+                    if ($event->getAction() === PlayerInteractEvent::LEFT_CLICK_BLOCK) {
+                        $player->sendMessage(Plugin::$prefix . Translation::translate('check-help', $amount));
+                    } else {
+                        $item->count = 1;
+                        $inventory->removeItem($item);
 
-                    $economyApi = EconomyAPI::getInstance();
-                    $economyApi->addMoney($player, $amount);
-                    $player->sendMessage(Plugin::$prefix . Translation::translate('check-use', $amount, $economyApi->myMoney($player)));
+                        $economyApi = EconomyAPI::getInstance();
+                        $economyApi->addMoney($player, $amount);
+                        $player->sendMessage(Plugin::$prefix . Translation::translate('check-use', $amount, $economyApi->myMoney($player)));
+                    }
                 }
             }
         }

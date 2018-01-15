3103cc75854f60ffe0ac472d68220ae81e18c855
PresentKim <qjrmckd@gmail.com>
2018-01-15 11:18:31 +0900

    Listen use the check in player interact event

--- src/presentkim/writecheck/listener/PlayerEventListener.php
+++ src/presentkim/writecheck/listener/PlayerEventListener.php
@@ -5,7 +5,12 @@ namespace presentkim\writecheck\listener;
 use pocketmine\event\{
   Listener, player\PlayerInteractEvent
 };
-use presentkim\writecheck\WriteCheckMain as Plugin;
+use pocketmine\item\Item;
+use pocketmine\nbt\tag\IntTag;
+use onebone\economyapi\EconomyAPI;
+use presentkim\writecheck\{
+  WriteCheckMain as Plugin, util\Translation
+};
 
 class PlayerEventListener implements Listener{
 
@@ -18,6 +23,21 @@ class PlayerEventListener implements Listener{
 
     /** @param PlayerInteractEvent $event */
     public function onPlayerInteractEvent(PlayerInteractEvent $event){
+        if ($event->getAction() === PlayerInteractEvent::RIGHT_CLICK_AIR) {
+            $player = $event->getPlayer();
+            $inventory = $player->getInventory();
+            $item = $inventory->getItemInHand();
+            if ($item->getId() == Item::PAPER && $item->getDamage() === 0xff) {
+                $amount = $item->getNamedTag()->getTagValue('whitecheck-amount', IntTag::class, -1);
+                if ($amount !== -1) {
+                    $item->count = 1;
+                    $inventory->removeItem($item);
 
+                    $economyApi = EconomyAPI::getInstance();
+                    $economyApi->addMoney($player, $amount);
+                    $player->sendMessage(Plugin::$prefix . Translation::translate('check-use', $amount, $economyApi->myMoney($player)));
+                }
+            }
+        }
     }
 }

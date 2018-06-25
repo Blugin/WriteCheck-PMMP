bab97665f5370383fdd364d00b927c3880d3da34
PresentKim <qjrmckd@gmail.com>
2018-06-26 00:47:29 +0900

    Merge write subcommand to main command, Listen command execute in onCommand()

--- src/kim/present/writecheck/WriteCheck.php
+++ src/kim/present/writecheck/WriteCheck.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace kim\present\writecheck;
 
-use kim\present\writecheck\command\PoolCommand;
-use kim\present\writecheck\command\subcommands\WriteSubCommand;
 use kim\present\writecheck\listener\PlayerEventListener;
 use kim\present\writecheck\util\{
 	Translation, Utils
 };
+use onebone\economyapi\EconomyAPI;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
 use pocketmine\item\Item;
 use pocketmine\nbt\tag\IntTag;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
 class WriteCheck extends PluginBase{
@@ -21,16 +24,16 @@ class WriteCheck extends PluginBase{
 	private static $instance = null;
 
 	/**
-	 * @var PoolCommand
-	 */
-	private $command = null;
-
-	/**
 	 * @return WriteCheck
 	 */
 	public static function getInstance() : WriteCheck{
 		return self::$instance;
 	}
+
+	/**
+	 * @var PluginCommand
+	 */
+	private $command;
 
 	public function onLoad() : void{
 		if(self::$instance === null){
@@ -56,15 +59,51 @@ class WriteCheck extends PluginBase{
 		}
 
 		//Register main command
-		$this->command = new PoolCommand($this, 'wcheck');
-		$this->command->createSubCommand(WriteSubCommand::class);
-		$this->command->updateTranslation();
-		$this->command->updateSudCommandTranslation();
-		$this->getServer()->getCommandMap()->unregister($this->command);
-		$this->getServer()->getCommandMap()->register(strtolower($this->getName()), $this->command);
+		$this->command = new PluginCommand(Translation::translate("command-wcheck"), $this);
+		$this->command->setPermission("wcheck.cmd");
+		$this->command->setAliases(Translation::getArray("command-wcheck@aliases"));
+		$this->command->setUsage(Translation::translate("command-wcheck@usage"));
+		$this->command->setDescription(Translation::translate("command-wcheck@description"));
+		$this->getServer()->getCommandMap()->register($this->getName(), $this->command);
 
 		//Register event listeners
 		$this->getServer()->getPluginManager()->registerEvents(new PlayerEventListener(), $this);
+	}
+
+	/**
+	 * @param CommandSender $sender
+	 * @param Command       $command
+	 * @param string        $label
+	 * @param string[]      $args
+	 *
+	 * @return bool
+	 */
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		if(isset($args[0]) && is_numeric($args[0])){
+			if($sender instanceof Player){
+				$amount = (int) $args[0];
+				$count = max(isset($args[1]) && is_numeric($args[1]) ? (int) $args[1] : 1, 1);
+
+				$economyApi = EconomyAPI::getInstance();
+				$price = $amount * $count;
+				if(($money = $economyApi->myMoney($sender)) < $price){
+					$sender->sendMessage(Translation::translate("command-wcheck@failure", (string) $money));
+				}else{
+					$return = $economyApi->reduceMoney($sender, $price, false, $this->getName());
+					if($return === EconomyAPI::RET_SUCCESS){
+						$sender->getInventory()->addItem($this->getCheck($amount, $count));
+						$sender->sendMessage(Translation::translate("command-wcheck@success", (string) $amount, (string) $count, (string) $price, (string) ($money - $price)));
+					}else{
+						$sender->sendMessage(Translation::translate("command-generic-failure@economy-failure", (string) $return));
+					}
+				}
+			}else{
+				$sender->sendMessage(Translation::translate('command-generic-failure@in-game'));
+			}
+			return true;
+		}else{
+			return false;
+		}
 	}
 
 	/**

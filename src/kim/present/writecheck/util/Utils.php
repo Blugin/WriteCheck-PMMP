f678c31b5c86f1633e5efa5f16a802a2dd8d8882
PresentKim <qjrmckd@gmail.com>
2018-06-26 01:29:44 +0900

    Utils : Remove in_arrayi() and toInt() method

--- src/kim/present/writecheck/util/Utils.php
+++ src/kim/present/writecheck/util/Utils.php
@@ -6,21 +6,6 @@ namespace kim\present\writecheck\util;
 
 class Utils{
 	/**
-	 * @param string $str
-	 * @param array  $strs
-	 *
-	 * @return bool
-	 */
-	public static function in_arrayi(string $str, array $strs) : bool{
-		foreach($strs as $key => $value){
-			if(strcasecmp($str, $value) === 0){
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/**
 	 * @param Object[] $list
 	 *
 	 * @return string[]
@@ -32,30 +17,5 @@ class Utils{
 			$pairs["{%$i}"] = $list[$i];
 		}
 		return $pairs;
-	}
-
-	/**
-	 * @param string        $str
-	 * @param int|null      $default = null
-	 *
-	 * @param \Closure|null $filter
-	 *
-	 * @return int|null
-	 */
-	public static function toInt(string $str, int $default = null, \Closure $filter = null) : ?int{
-		if(is_numeric($str)){
-			$i = (int) $str;
-		}elseif(is_numeric($default)){
-			$i = $default;
-		}else{
-			return null;
-		}
-		if(!$filter){
-			return $i;
-		}elseif($result = $filter($i)){
-			return $result === -1 ? $default : $i;
-		}else{
-			return null;
-		}
 	}
 }
